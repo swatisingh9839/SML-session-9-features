455cbbd1ac3cce9204dbaf6fced3cd18403071eb
Swati Singh <69922265+swatisingh9839@users.noreply.github.com>
2020-11-25 15:56:31 +0100

    Create features.py

---
+++ features.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sklearn.linear_model as skl_lm
+
+from matplotlib.patches import Arc
+
+import itertools
+import math
+
+mpl.rcParams['axes.spines.right'] = False
+mpl.rcParams['axes.spines.top'] = False
